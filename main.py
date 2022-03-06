bf5e6843ab08f301f37dffd06c90684a3fc13701
Magdalena <228932@edu.p.lodz.pl>
2022-03-06 21:09:27 +0100

    WIP - Depth-First Search (not working)

--- main.py
+++ main.py
@@ -6,13 +6,6 @@ model_matrix = [[1, 2, 3, 4],
                 [13, 14, 15, 0]]
 
 
-def generate_random_matrix():
-    list_of_numbers = list(range(0, 16))
-    random.shuffle(list_of_numbers)
-    matrix = [list_of_numbers[0:4], list_of_numbers[4:8], list_of_numbers[8:12], list_of_numbers[12:16]]
-    return matrix
-
-
 def check_valid(matrix):
     if matrix == model_matrix:
         return True
@@ -20,4 +13,62 @@ def check_valid(matrix):
         return False
 
 
+def generate_random_matrix():
+    list_of_numbers = list(range(0, 16))
+    random.shuffle(list_of_numbers)
+    matrix = [list_of_numbers[0:4], list_of_numbers[4:8], list_of_numbers[8:12], list_of_numbers[12:16]]
+    return matrix
+
+
+class Node:
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+        self.visited = False
+
+    def __add__(self, node):
+        if self.data > node.data:
+            self.left = node
+        else:
+            self.right = node
+
+
+def DFS(node,root):
+    node.visited = True
+    ancestor = node
+    if node.left is not None and node.left.visited is not True:
+        print('lewo \n')
+        DFS(node.left, root)
+    elif node.right is not None and node.right.visited is not True:
+        print('prawo \n')
+        DFS(node.right, root)
+    else:
+        if node == root:
+            return None
+        else:
+            print('wracam \n')
+            DFS(ancestor, root)
+
+
 print(generate_random_matrix())
+
+#przykładowe drzewo dla sprawdzenia poparwności działania algorytmu DFS
+#               3
+#      2                    6
+#          4            5       7
+#       1       11    0           13
+#
+#zgodnie z DFS powinno byc
+node1=Node(1)
+node11=Node(11)
+node0=Node(0)
+node13=Node(13)
+node4=Node(4,node1,node11)
+node5=Node(5,node0)
+node7=Node(7,None,node13)
+node2=Node(2,None,node4)
+node6=Node(6,node5,node7)
+node3=Node(3,node2,node4)
+
+DFS(node3, node3)
