59ecfe94eced9f46f6cb486a5363b95cacf92166
Magdalena <228932@edu.p.lodz.pl>
2022-03-06 18:48:41 +0100

    add function to generate matrix with numbers in range from 0 to 15, where 0 is considering as blank field

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+model_matrix = [[1, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 10, 11, 12],
+                [13, 14, 15, 0]]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def generate_random_matrix():
+    list_of_numbers = list(range(0, 16))
+    random.shuffle(list_of_numbers)
+    matrix = [list_of_numbers[0:4], list_of_numbers[4:8], list_of_numbers[8:12], list_of_numbers[12:16]]
+    return matrix
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print(generate_random_matrix())
