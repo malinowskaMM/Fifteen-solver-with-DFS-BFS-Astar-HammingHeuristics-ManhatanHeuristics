2468b2369e05d729c3d2e886e60f173d68f07395
Michal Banasiak <bananiwo@github.com>
2022-03-10 21:32:03 +0100

    DFS wip

--- main.py
+++ main.py
@@ -30,21 +30,22 @@ def check(board, solvedBoard):
     else:
         return False
 
-counter = 0
-def createTree(root):
-    while counter < 20:
-        root.move('L')
+# counter = 0
+# def createTree(root):
+#     while counter < 20:
+#         root.move('L')
 
 
 class Node:
     def __init__(self, board, parent, birthMove):
         self.board = board
-        self.isBoardCorrect = check(self.board, STARTBOARD)
+        self.isBoardCorrect = check(self.board, SOLVEDBOARD)
         # self.children = []
         self.childLeft = None
         self.childRight = None
         self.childUp = None
         self.childDown = None
+        self.birthMove = None
         if parent is not None:  # None is reserved for ROOT node
             self.parent = parent
             self.birthMove = birthMove
@@ -57,56 +58,57 @@ class Node:
     def move(self, move):
         yPos = BLANK['row']
         xPos = BLANK['col']
-        if move == 'L':
-            if xPos == 0:
-                return 0        # Ending branch on left
+        if (move == 'L'):
+            if xPos == 0 | (self.birthMove == 'R'):
+                return None        # Ending branch on left
             BLANK['col'] -= 1
             tempBoard = self.board
             tempBoard[yPos][xPos] = tempBoard[yPos][xPos-1]
             tempBoard[yPos][xPos-1] = '0'
             print(tempBoard)
-            self.childLeft = self.makeChild(tempBoard, move, 'L')
-        if move == 'R':
-            if xPos == 3:
-                return 0        # Ending branch on right
+            self.childLeft = self.makeChild(tempBoard, move)
+            return self.childLeft
+        if move == 'R' :
+            if xPos == 3 | (self.birthMove == 'L'):
+                return None        # Ending branch on right
             BLANK['col'] += 1
             tempBoard = self.board
             tempBoard[yPos][xPos] = tempBoard[yPos][xPos+1]
             tempBoard[yPos][xPos+1] = '0'
             print(tempBoard)
-            self.childRight = self.makeChild(tempBoard, move, 'R')
-        if move == 'U':
-            if yPos == 3:
-                return 0        # Ending branch up
+            self.childRight = self.makeChild(tempBoard, move)
+            return self.childRight
+        if move == 'U' :
+            if yPos == 3 | (self.birthMove == 'D'):
+                return None        # Ending branch up
             BLANK['row'] -= 1
             tempBoard = self.board
             tempBoard[yPos][xPos] = tempBoard[yPos-1][xPos]
             tempBoard[yPos-1][xPos] = '0'
             print(tempBoard)
-            self.childUp = self.makeChild(tempBoard, move, 'U')
+            self.childUp = self.makeChild(tempBoard, move)
+            return self.childUp
         if move == 'D':
-            if yPos == 0:
-                return 0        # Ending branch down
+            if yPos == 0 | (self.birthMove == 'U'):
+                return None        # Ending branch down
             BLANK['row'] += 1
             tempBoard = self.board
             tempBoard[yPos][xPos] = tempBoard[yPos+1][xPos]
             tempBoard[yPos+1][xPos] = '0'
             print(tempBoard)
-            self.childDown = self.makeChild(tempBoard, move, 'D')
+            self.childDown = self.makeChild(tempBoard, move)
+            return self.childDown
 
-
-def DFS(node):
+def DFS(node, counter):
     # node.visited = True
-    if not node.isBoardCorrect:
-        DFS(node.childLeft)
-        DFS(node.childRight)
-        DFS(node.childUp)
-        DFS(node.childDown)
-
-
-# counter = 0
-# def createTree(root):
-#     while counter < MAXDEPTH:
+    if node is not None:
+        if not node.isBoardCorrect:
+            if counter < 20:
+                counter += 1
+                DFS(node.move('L'), counter)
+                DFS(node.move('R'), counter)
+                DFS(node.move('U'), counter)
+                DFS(node.move('D'), counter)
 
 
 
@@ -129,7 +131,7 @@ if __name__ == '__main__':
     readFromFileToBoard()
     findField(STARTBOARD)
     root = Node(STARTBOARD, None, None)
-    DFS(root)
+    DFS(root, 0)
     # root.move('L')
     # root.move('R')
     # root.move('U')
