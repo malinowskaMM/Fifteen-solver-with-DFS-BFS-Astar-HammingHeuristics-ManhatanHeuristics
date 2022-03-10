327bd5ea867e99434fb2945706d28c24128985b0
Magdalena <228932@edu.p.lodz.pl>
2022-03-10 20:42:03 +0100

    dfs

--- main.py
+++ main.py
@@ -32,23 +32,29 @@ def check(board, solvedBoard):
 class Node:
     def __init__(self, board, parent):
         self.board = board
+        self.children = []
         if parent is not None:  # None is reserved for ROOT node
             self.parent = parent
 
     def makeChild(self, board, move):
         child = Node(board, self)
+        self.children.append(child)
 
     def move(self, move):
         yPos=BLANK['row']
         xPos=BLANK['col']
         if move == 'L':
+            if xPos == 0:
+                return 0        # Ending branch on left
             BLANK['col'] -= 1
             tempBoard = self.board
             tempBoard[yPos][xPos] = tempBoard[yPos][xPos-1]
             tempBoard[yPos][xPos-1] = '0'
             print(tempBoard)
             self.makeChild(tempBoard, move)
-        if move == 'P':
+        if move == 'R':
+            if xPos == 3:
+                return 0        # Ending branch on right
             BLANK['col'] += 1
             tempBoard = self.board
             tempBoard[yPos][xPos] = tempBoard[yPos][xPos+1]
@@ -56,6 +62,8 @@ class Node:
             print(tempBoard)
             self.makeChild(tempBoard, move)
         if move == 'U':
+            if yPos == 3:
+                return 0        # Ending branch up
             BLANK['row'] -= 1
             tempBoard = self.board
             tempBoard[yPos][xPos] = tempBoard[yPos-1][xPos]
@@ -63,12 +71,27 @@ class Node:
             print(tempBoard)
             self.makeChild(tempBoard, move)
         if move == 'D':
+            if yPos == 0:
+                return 0        # Ending branch down
             BLANK['row'] += 1
             tempBoard = self.board
             tempBoard[yPos][xPos] = tempBoard[yPos+1][xPos]
             tempBoard[yPos+1][xPos] = '0'
             print(tempBoard)
             self.makeChild(tempBoard, move)
+
+
+def dfs():
+    root=Node(STARTBOARD, None)
+    continueFlag = True
+    while continueFlag:
+        if check(STARTBOARD, SOLVEDBOARD):
+            continueFlag = False
+        else:
+            root.move('L')
+            root.move('R')
+            root.move('U')
+            root.move('D')
 
 
 
@@ -78,6 +101,6 @@ if __name__ == '__main__':
     findField(STARTBOARD)
     root = Node(STARTBOARD,None)
     root.move('L')
-    root.move('P')
+    root.move('R')
     root.move('U')
     root.move('D')
