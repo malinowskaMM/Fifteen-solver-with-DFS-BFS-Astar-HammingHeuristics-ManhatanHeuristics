dd388711632f37d4b90c376436b6f19fd4638884
Magdalena <228932@edu.p.lodz.pl>
2022-03-29 20:39:09 +0200

    fix astar

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import copy
-import heapq
 import sys
 import time
 
@@ -59,7 +58,9 @@ class Node:
         self.children = []
         self.visited = False
         self.depthCounter = 0
-        self.cost = 0
+        self.gCost = 0
+        self.hCost = 0
+        self.totalCost = 0
 
     def makeChild(self, board, birthMove):
         child = Node(board, self, birthMove)
@@ -120,32 +121,6 @@ class Node:
             if self.birthMove == "U":
                 return None
             self.move(move)
-
-    def backMove(self):
-        findZero(self.board)
-        y = BLANK['row']
-        x = BLANK['col']
-        if self.birthMove == 'L':
-            if x == 3:
-                return None
-            self.board[y][x], self.board[y][x + 1] = self.board[y][x + 1], self.board[y][x]
-        elif self.birthMove == 'R':
-            if x == 0:
-                return None
-            self.board[y][x], self.board[y][x - 1] = self.board[y][x - 1], self.board[y][x]
-        elif self.birthMove == 'U':
-            if y == 3:
-                return None
-            self.board[y][x], self.board[y + 1][x] = self.board[y + 1][x], self.board[y][x]
-        elif self.birthMove == 'D':
-            if y == 0:
-                return None
-            self.board[y][x], self.board[y - 1][x] = self.board[y - 1][x], self.board[y][x]
-        self.children.clear()
-        self.leftChild = None
-        self.rightChild = None
-        self.upChild = None
-        self.downChild = None
 
 
 def dfs(node, way, visitedStates, processedStates, startTime, depthCounter=0):
@@ -229,33 +204,40 @@ def hammingDist(matrix, modelMatrix):
     return diffCounter
 
 
-def heapsort(iterable):
-    h = []
-    for value in iterable:
-        heapq.heappush(h, value)
-    return [heapq.heappop(h) for i in range(len(h))]
-
 
 def astar(node, heuristic, way, startTime, processedStates=0, visitedStates=0, depthCounter=0):
-    h = []
-    while check(node.board, SOLVEDBOARD) is False:
+    openList = []
+    closedList = []
+    openList.append(node)
+    while openList:
+        currentNode = min(openList, key=lambda node: node.totalCost)
+        openList.remove(currentNode)
+        closedList.append(currentNode)
+
+        if check(currentNode.board, SOLVEDBOARD) is True:
+            print(currentNode.board)
+            return [node.board, way, visitedStates, processedStates, depthCounter, time.time_ns() - startTime]
+
         for o in ORDER:
-            node.restrictMovement(o)
+            currentNode.restrictMovement(o)
             visitedStates += 1
-        for child in node.children:
-            cost = 0
+        for child in currentNode.children:
+            child.gCost = currentNode.gCost + 1
             if heuristic == "manh":
-                cost = depthCounter + manhattanDist(child.board, SOLVEDBOARD)
+                child.hCost = manhattanDist(child.board, SOLVEDBOARD)
             if heuristic == "hamm":
-                cost = depthCounter + hammingDist(child.board, SOLVEDBOARD)
-            heapq.heappush(h, (cost, copy.deepcopy(child.board), child.birthMove))
-            child.backMove()
-        heapsort(h)
-        result = heapq.heappop(h)
-        node.board = result[1]
-        way.append(result[2])
-        processedStates += 1
-        depthCounter += 1
+                child.hCost = hammingDist(child.board, SOLVEDBOARD)
+            child.totalCost = child.gCost + child.hCost
+
+            if child in openList:
+                if child.gCost > min(openList, key=lambda node: node.totalCost).gCost:
+                    continue
+            processedStates += 1
+            depthCounter += 1
+            way.append(child.birthMove)
+            print(child.board)
+            openList.append(child)
+
     return [node.board, way, visitedStates, processedStates, depthCounter, time.time_ns() - startTime]
 
 
@@ -289,6 +271,7 @@ def writeSolution(fileName, result):
     for i in range(0, len(sol)):
         way += sol[i]
     file.write(way)
+
 
 if __name__ == '__main__':
     readFromFileToBoard(sys.argv[3])
