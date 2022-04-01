db3b91d58a993ac94f0be2f5550ca7e400bf456e
Michał <bananiwo@gmail.com>
2022-04-02 01:18:00 +0200

    write and read csv data, draw plot

--- writeStats.py
+++ writeStats.py
@@ -3,11 +3,10 @@ from os import listdir
 import time
 import cProfile
 
-m.ORDER = ['U', 'L', 'R', 'D']
-MAXDEPTH = 20
+m.ORDER = ['D', 'R', 'L', 'U']
 
 inputPath = 'F:\Studia\sem4\SISE\Projekt_SISE\puzzles'
-outputFilename = 'F:\Studia\sem4\SISE\Projekt_SISE\output_astar_manh.csv'
+outputFilename = 'F:\Studia\sem4\SISE\Projekt_SISE\output_DFS_DRLU.csv'
 inputFiles = listdir(inputPath)
 
 outputFile = open(outputFilename, 'a')
@@ -16,10 +15,11 @@ for inputFile in inputFiles:
     counter += 1
     m.STARTBOARD = []
     m.STARTBOARD = m.readFromFileToBoard('puzzles/'+inputFile)
+    # m.STARTBOARD = m.readFromFileToBoard('puzzles/4x4_03_00004.txt')
     m.BLANK = m.findZero(m.STARTBOARD)
     root = m.Node(m.STARTBOARD)
 
-    solution = m.astar(root, 'manh', [], time.time_ns())
+    solution = m.dfs(root, time.time_ns())
     wayLen = len(solution[1])
     visitedStatesCount = solution[2]
     processedStatesCount = solution[3]
@@ -27,8 +27,8 @@ for inputFile in inputFiles:
     elapsedTime = "{:.3f}".format(solution[5] / 1000000)
 
     line = f'{inputFile},{wayLen},{visitedStatesCount},{processedStatesCount},{depth},{elapsedTime}'
-    outputFile.write('\n')
     outputFile.write(line)
+    outputFile.write('\n')
     print("Rozwiązanych plansz:", counter)
 
 outputFile.close()
