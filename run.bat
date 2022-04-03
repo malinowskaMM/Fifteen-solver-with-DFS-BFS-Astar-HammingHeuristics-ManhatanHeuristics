7329556e3cbeca6eb4d1beece9c64f936f0aa79d
Magdalena <228932@edu.p.lodz.pl>
2022-04-03 17:44:39 +0200

    add stats

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 set codedirectory=D:\SISE\puzzles
 for /r  %codedirectory% %%i in (*.txt) do (
-python main.py astar hamm %%i 4x4_07_00212_sol.txt 4x4_07_00212_stats.txt)
+python main.py dfs LUDR %%i 4x4_07_00212_sol.txt 4x4_07_00212_stats.txt)
